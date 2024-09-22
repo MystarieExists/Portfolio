5fb48ab91ef30925b0e19c577fc8a1ddbdd162b5
Mahdi Chowdhury <mc5u22@soton.ac.uk>
2024-09-22 11:36:50 +0100

    Initial push

---
+++ assets/js/projects.js
@@ -0,0 +1,72 @@
+$(document).ready(() => {
+    render_projects('featured');
+})
+
+
+let render_projects = (slug) => {
+    let projects_area = $('.projects-wrapper');
+
+    $('.white-button').removeClass('white-button-hover');
+    $(`#${slug}`).addClass('white-button-hover');
+
+    let projects_obj = [
+        {
+            image: 'assets/images/background.png',
+            link: 'Test text',
+            title: 'Test text',
+            demo: '',
+            technologies: ['Python'],
+            description: "Test text",
+            categories: ['all']
+        },
+    ]
+
+    let projects = [];
+    if(slug == 'all') {
+        projects = projects_obj.map(project_mapper);
+    } 
+    else {
+        projects = projects_obj.filter(project => project.categories.includes(slug)).map(project_mapper);
+    }
+    projects_area.hide().html(projects).fadeIn();
+}
+
+let project_mapper = project => {
+    return `
+        <div class="wrapper">
+                
+            <div class="card radius shadowDepth1">
+
+                ${project.image ? 
+                    `<div class="card__image border-tlr-radius">
+                        <a href="${project.link}">
+                            <img src="${project.image}" alt="image" id="project-image" class="border-tlr-radius">
+                        </a>
+                    </div>`           
+                : ''}
+
+        
+                <div class="card__content card__padding">
+        
+                    <article class="card__article">
+                        <h2><a href="${project.link}">${project.title}</a></h2>
+        
+                        <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}">Demo</a>` : ''}</p>
+                    </article>
+
+                                
+                    <div class="card__meta">
+                        ${project.technologies.map(tech =>
+                            `<span class="project-technology paragraph-text-normal">${tech}</span>`
+                        ).join('')}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `
+}
+
+let selected = (slug) => {
+    render_projects(slug);
+}
